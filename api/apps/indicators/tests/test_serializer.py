8df24fbce9cf549e47e7f586e38924956bdbcf94
Rafael Torres <rdtr.sis@gmail.com>
2017-09-14 15:27:04 -0300

    fix: Added setting to Pytest

---
+++ api/apps/indicators/tests/test_serializer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sep 14, 2017
+
+@author: rtorres
+'''
+import datetime
+
+from django.test.testcases import TestCase
+from rest_framework_json_api.utils import format_resource_type
+
+from apps.indicators.models import UF
+from apps.indicators.serializers import UFSerializer
+
+
+class TestUfSerializer(TestCase):
+
+    def setUp(self):
+        self.uf = UF.objects.create(
+            date=datetime.date.today(),
+            value=200.09)
+
+    def test_data_rigth_format(self):
+        serializer = UFSerializer(isntance=self.uf)
+        expected_data = {
+            'type': format_resource_type('UF'),
+            'id': str(self.uf.id),
+            'attributtes': {
+                'date': datetime.date.today().strftime('%y-%m-%d'),
+                'value': '200.09'}}
+        assert serializer.data == expected_data
---
+++ api/conf/settings/test.py
@@ -0,0 +1,52 @@
+'''
+Test settings
+
+- Used to run tests fast on the continuous integration server and locally
+'''
+
+from .base import *  # noqa
+
+# DEBUG
+# ------------------------------------------------------------------------------
+# Turn debug off so tests run faster
+DEBUG = False
+TEMPLATES[0]['OPTIONS']['debug'] = False  # noqa
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = 'CHANGEME!!!'
+
+
+# CACHING
+# ------------------------------------------------------------------------------
+# Speed advantages of in-memory caching without having to run Memcached
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': '',
+    },
+}
+
+# TESTING
+# ------------------------------------------------------------------------------
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+
+# PASSWORD HASHING
+# ------------------------------------------------------------------------------
+# Use fast password hasher so tests run faster
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# TEMPLATE LOADERS
+# ------------------------------------------------------------------------------
+# Keep templates in memory so tests run faster
+TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa
+    ['django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ], ],
+]
